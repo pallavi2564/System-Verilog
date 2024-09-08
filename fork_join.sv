586b3e7c664470af7fd1aa0f54cb6070f00b555a
Pallavi <yampalakupallavi@gmail.com>
2024-09-08 15:23:56 +0530

    Create fork_join.sv

---
+++ fork_join.sv
@@ -0,0 +1,18 @@
+module fork_join;
+  initial begin
+    fork
+      begin
+        $display($time,"\tProcess-1 Started");
+        #5;
+        $display($time,"\tProcess-1 Finished");
+      end
+      begin
+        $display($time,"\tProcess-2 Started");
+        #20;
+        $display($time,"\tProcess-2 Finished");
+      end
+    join
+    $display($time,"\tOutside Fork-Join");
+    $finish;
+  end
+endmodule
